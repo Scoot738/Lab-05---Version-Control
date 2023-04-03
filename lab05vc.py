2c66300707fd00afc48a19c4e3049eccc561054e
Scoot738 <cman738@gmail.com>
2023-04-03 12:01:58 -0700

    Added a new function to the current script to find the average

--- lab05vc.py
+++ lab05vc.py
@@ -1,5 +1,13 @@
 def add_numbers(a, b):
 	return a + b
+    
+def calculate_average(numbers):
+    total = sum(numbers)
+    return total / len(numbers)
 	
 result = add_numbers(3, 5)
 print(result)
+
+numbers = [2, 4, 6, 8, 10]
+average = calculate_average(numbers)
+print(average)
